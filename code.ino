93ccb96f38ca59e1a4e23ecf35e6be4bbd255b2d
ylu <123523291+ylu8888@users.noreply.github.com>
2023-07-08 05:41:19 -0400

    Update code.ino

--- code.ino
+++ code.ino
@@ -6,10 +6,12 @@ const int motorOne_Enable = 9;
 const int motorTwo_Enable = 10;
 const int motorTwo_InputOne = 6;
 const int motorTwo_InputTwo = 7;
+
 const int trig = 8;
 const int echo = 12;
 const int buzzer = 2;
 const int laserPin = 13;
+
 int switchPin = 3;
 int switchState = 0;
 int duration;
@@ -24,23 +26,29 @@ void setup()
  pinMode(laserPin, OUTPUT);
  pinMode(4, OUTPUT);
  pinMode(buzzer, OUTPUT);
+ 
  pinMode(5, OUTPUT);
  pinMode(9, OUTPUT);
  pinMode(10, OUTPUT);
  pinMode(6, OUTPUT);
  pinMode(7, OUTPUT);
+ 
  pinMode(trig, OUTPUT);
  pinMode(echo, INPUT);
  pinMode(switchPin, INPUT);
+ 
 digitalWrite(motorOne_Enable, HIGH);
 digitalWrite(motorTwo_Enable, HIGH);
 digitalWrite(laserPin, LOW);
+
+ 
 for(int degree = 179; degree >= 0; degree--)
   {
     myServo.write(degree);
     delay(5);
 
   }
+ 
 }
  
 void loop()
@@ -48,6 +56,7 @@ void loop()
  pinMode(trig, OUTPUT);
  digitalWrite(trig, LOW);
  delayMicroseconds(2);
+
  digitalWrite(trig, HIGH);
  delayMicroseconds(5);
  digitalWrite(trig, LOW);
@@ -57,6 +66,7 @@ void loop()
 switchState = digitalRead(switchPin);
 
  distance = duration*0.034/2;
+ 
 if (switchState == HIGH)
 {
 digitalWrite(motorOne_InputOne, HIGH);
@@ -82,27 +92,35 @@ if (distance <= 10)
   tone(buzzer, 200);
   digitalWrite(laserPin, HIGH);
   delay(500);
+ 
   noTone(buzzer);
   digitalWrite(laserPin, LOW);
   delay(500);
+ 
   tone(buzzer, 400);
   digitalWrite(laserPin, HIGH);
   delay(500);
+ 
   noTone(buzzer);
   digitalWrite(laserPin, LOW);
   delay(500);
+ 
   tone(buzzer, 600);
   digitalWrite(laserPin, HIGH);
   delay(500);
+ 
   noTone(buzzer);
   digitalWrite(laserPin, LOW);
   delay(500);
 
+ 
  for(int degree = 179; degree >= 0; degree--)
   {
     myServo.write(degree);
     delay(5);
   }
+
+ 
 digitalWrite(motorOne_InputOne, HIGH);
 digitalWrite(motorOne_InputTwo, LOW);
 digitalWrite(motorTwo_InputOne, HIGH);
